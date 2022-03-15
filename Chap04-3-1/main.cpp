dd82fc061a323d7347d6da47fd9b435d73151f14
LSH3333 <dltpgustpgus@gmail.com>
2022-03-15 17:46:04 +0900

    2022.03.15

--- Chap04-3-1/main.cpp
+++ Chap04-3-1/main.cpp
@@ -1,6 +1,68 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+class Point
+{
+private:
+    int xpos, ypos;
+
+public:
+    Point(int x, int y): xpos(x), ypos(y) {}
+
+    void ShowPointInfo() const
+    {
+        cout << "[" << xpos << ", " << ypos << "]" << endl;
+    }
+
+    int GetX() const
+    {
+        return xpos;
+    }
+
+    int GetY() const
+    {
+        return ypos;
+    }
+};
+
+class Circle
+{
+private:
+    Point center;
+    int rad;
+
+public:
+    Circle(int x, int y, int _rad, Point center)
+    : center(Point(x,y)), rad(_rad)
+    {}
+
+    void ShowCircleInfo() const
+    {
+        cout << "radius: " << rad << endl;
+        center.ShowPointInfo();
+    }
+};
+
+class Ring
+{
+private:
+    Circle innerCircle;
+    Circle outterCircle;
+
+public:
+
+    void ShowRingInfo()
+    {
+        cout << "Inner Circle Info..." << endl;
+        innerCircle.ShowCircleInfo();
+        cout << "Outter Circle Info..." << endl;
+        outterCircle.ShowCircleInfo();
+    }
+};
+
+//int main()
+//{
+//    Ring ring;
+//    ring.Init(1, 1, 4, 2, 2, 9);
+//    ring.ShowRingInfo();
+//}
