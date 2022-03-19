3048282b610752bbc0f749e89d9d5f52ad8c4660
LSH <dltpgustpgus@gmail.com>
2022-03-19 16:22:09 +0900

    2022.03.19

---
+++ Chap08/cmake-build-debug/CMakeFiles/Chap08.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chap08.dir/main.cpp.obj"
+  "Chap08.exe"
+  "Chap08.exe.manifest"
+  "Chap08.pdb"
+  "libChap08.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chap08.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
