e3a860b32c857e641f0ae4999bb97ad8f61a0d44
LSH <dltpgustpgus@gmail.com>
2022-03-28 22:48:31 +0900

    2022.03.28

---
+++ Chap16/cmake-build-debug/CMakeFiles/Chap16.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chap16.dir/main.cpp.obj"
+  "CMakeFiles/Chap16.dir/main.cpp.obj.d"
+  "Chap16.exe"
+  "Chap16.exe.manifest"
+  "Chap16.pdb"
+  "libChap16.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chap16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Chap16/main.cpp
+++ Chap16/main.cpp
@@ -3,15 +3,53 @@ using namespace std;
 
 class Car
 {
-
+private:
+    int fuelGauge;
+public:
+    Car(int fuel) : fuelGauge(fuel) {}
+    void ShowCarState() { cout << "잔여 연료량: " << fuelGauge << endl; }
 };
 
 class Truck : public Car
 {
+private:
+    int freightWeight;
+public:
+    Truck(int fuel, int weight) : Car(fuel), freightWeight(weight) {}
+    void ShowTruckState()
+    {
+        ShowCarState();
+        cout << "화물의 무게: " << freightWeight << endl;
+    }
+};
 
+void ShowString(char *str)
+{
+    cout << str << endl;
+}
+
+// Polymorphic 클래스
+class SoSimple
+{
+public:
+    virtual void ShowSimpleInfo() // 가상함수
+    {
+        cout << "SoSimple Class" << endl;
+    }
+};
+
+class SoComplex : public SoSimple
+{
+public:
+    void ShowSimpleInfo() // 오버라이딩된 가상함수는 자동으로 가상함수
+    {
+        cout << "SoComplex Derived Class" << endl;
+    }
 };
 
 int main()
 {
-    Car *pcar1 = new Truck();
+    SoSimple *simPtr = new SoComplex;
+    SoComplex *comPtr = dynamic_cast<SoComplex*>(simPtr);
+    comPtr->ShowSimpleInfo();
 }
